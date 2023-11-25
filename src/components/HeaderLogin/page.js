bf66cedbae65b79710a0d5d7d852e28db320abec
leonardo zambrana <leonardozambrana1@gmail.com>
2023-11-24 22:14:22 -0300

    Primeiro-Commit

---
+++ src/components/HeaderLogin/page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HomeHeader = () => {
+    return (
+        <header className='bg-black'>
+            <div className='flex justify-between px-20 py-10 text-white'>
+                <span>Artigos</span>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href='/Login'>Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default HomeHeader;
